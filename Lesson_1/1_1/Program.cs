f3f998443402c88651e634080964f1b24d7f64a2
Елизавета Белобородова <Liza>
2023-12-26 21:25:45 +0500

    задача найти квадрат.корень

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Write("Enter a number 1: ");
+int b1 = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Enter a number 2: ");
+int b2 = Convert.ToInt32(Console.ReadLine()!);
+
+if (b2 * b2 == b1)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
