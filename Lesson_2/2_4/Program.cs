feaf32ca231f0a998735723c103b703b033a7e31
Елизавета Белобородова <Liza>
2023-12-31 13:02:06 +0500

    add lesson_2 task 2_4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4. Напишите программу, которая выводит третью с конца цифру заданного числа или сообщает
+// что третьей цифры нет.
+
+// 456 => 4
+// 7812 => 8
+// 91 => Третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100)
+    {
+        double result = num / 100 % 10;
+        Console.WriteLine(result);
+    }
+else 
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
