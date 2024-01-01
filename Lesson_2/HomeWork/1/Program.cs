e0a06accaed10d2afdcce13dc6d2520ab870a7d1
Елизавета Белобородова <Liza>
2024-01-01 16:13:13 +0500

    add change in .gitignore

--- Lesson_2/HomeWork/1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine()); 
-
-if (num % 7 == 0 && num % 23 == 0)
-    Console.WriteLine("cratno");
-else
-{
-    Console.WriteLine("necratno");
-}
