5e3067c448a0809bd76c531bd449f61ebd460dee
Елизавета Белобородова <Liza>
2024-01-01 17:25:05 +0500

    add lesson_2 HW 3

---
+++ Lesson_2/HomeWork/3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число от 10 до 99: ");
+int num = Convert.ToInt32(Console.ReadLine ());
+if ((num >= 10) && (num <= 99))
+{
+    if ((num / 10) > (num % 10))
+        Console.Write("Наибольшая цифра введённого числа: " + num / 10);
+    else if ((num / 10) == (num % 10))
+            Console.Write("Цифры в числе одинаковы");
+    else
+        Console.Write("Наибольшая цифра введённого числа: " + num % 10);
+}
+else
+    Console.Write("Введённое число за пределами требуемого диапазона");
