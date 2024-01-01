e02d8a61ec960bb298aa9aac092aabb95223c9ad
Елизавета Белобородова <Liza>
2024-01-01 17:56:17 +0500

    add lesson_2 HW 4

---
+++ Lesson_2/HomeWork/4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string set_of_num = "";
+if (num > 0)
+{
+    while (num > 0)
+    {
+        set_of_num = num % 10 + "," + set_of_num;
+        num = num / 10;
+    }
+    // set_of_num = set_of_num.Trim({ ',' });
+    Console.Write(set_of_num.Trim(',') + "\n");
+}
+else
+    Console.Write("Введенное число не является натуральным");
