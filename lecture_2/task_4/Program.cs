0479b08d26855655aefb074a757d3732ce4c8213
Елизавета Белобородова <Liza>
2024-01-11 22:49:14 +0500

    add lecture_2 task_4

---
+++ lecture_2/task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] array = {2, 5, 4, 8, 6};
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i++;
+}
+Console.WriteLine(max);
