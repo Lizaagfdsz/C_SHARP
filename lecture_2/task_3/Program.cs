d87ae7538d9f2e9f2521fb9b41558f542830bd5f
Елизавета Белобородова <Liza>
2024-01-11 22:42:18 +0500

    add lecture_2 task_3

---
+++ lecture_2/task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = {4, 5, 7, 10, 6, 3, 23, 89, 8, 36};
+int i = 0;
+
+while (i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i++;
+}
