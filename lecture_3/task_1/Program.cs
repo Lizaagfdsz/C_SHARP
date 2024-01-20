fa01c513b9bc690082add06c545dbf1a170b58fa
liza <li.belobor@yandex.ru>
2024-01-20 14:01:45 +0500

    add lecture_3 task_1

---
+++ lecture_3/task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// вычислить значение формулы (a*b)/(c+d)
+// где a,b,c,d - некоторые целые числа.
+// Решение оформить в виде функции.
+
+double CalculateFormula(int a, int b, int c, int d)
+// функции которые что то вычисляют, они имеют возвращаемое значение.
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
