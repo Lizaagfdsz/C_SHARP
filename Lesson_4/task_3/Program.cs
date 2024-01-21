db2511be0f8bb6f1bce1f158550c37b7fde08e06
Елизавета Белобородова <Liza>
2024-01-21 17:38:07 +0500

    add Lesson_4 task_3

---
+++ Lesson_4/task_3/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 3.
+// Заполните массив на N (вводится с консоли, не более 8)
+// случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из
+// массива. Старший разряд числа находится на 0-м индексе,
+// младший – на последнем.
+
+class Program{
+static void Main(){
+Console.WriteLine("enter array size (max 8)");
+int size = Convert.ToInt32(Console.ReadLine());
+Print(" ");
+
+
+if(size <= 8)
+{
+    int[] arr = GenerateRandomArray(size, 1, 10);
+Array.Sort(arr);
+Array.Reverse(arr);
+foreach(int num in arr){
+Print("element : " + num);
+}
+Print(" ");
+
+int res = MakeInteger(arr, size);
+Print("result : " + res);
+Print(" ");
+}
+else
+{
+Print("ERROR! Enter > 8");
+}
+
+static void Print(string res) { //функция выводящая результат на экран
+Console.WriteLine(res);
+}
+static int[] GenerateRandomArray(int size, int min_val, int max_val){
+Random random = new Random();
+int[] arr = new int[size];
+for(int i = 0; i < size; i++){
+arr[i] = random.Next(min_val, max_val);
+}
+return arr;
+}
+static int MakeInteger(int[] arr, int size){
+int result = 0;
+for(int i = 0; i < size; i++){
+result = result * 10 + arr[i];
+}
+return result;
+}
+}
+}
