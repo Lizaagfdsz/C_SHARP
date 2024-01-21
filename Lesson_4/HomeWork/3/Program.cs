4529731cd7557a4e025eb607c9ac16930caf7e2c
Елизавета Белобородова <Liza>
2024-01-21 22:35:11 +0500

    add HomeWork 3

---
+++ Lesson_4/HomeWork/3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создание массива
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        // Вывод исходного массива
+        Console.WriteLine("Исходный массив:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+
+        // Переворачивание массива
+        Array.Reverse(array);
+
+        // Вывод перевернутого массива
+        Console.WriteLine("\nПеревернутый массив:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
