a106b556e1d962bb329833f76a0a2e2470537550
liza <li.belobor@yandex.ru>
2024-02-08 22:20:21 +0500

    add lecture_4 4.1

---
+++ lecture_4/4.1/Program.cs
@@ -0,0 +1,52 @@
+﻿int[,] originalMatrix = new int[4,  5];
+
+Random rnd = new Random();
+for (int i =  0; i < originalMatrix.GetLength(0); i++)
+{
+    for (int j =  0; j < originalMatrix.GetLength(1); j++)
+    {
+        originalMatrix[i, j] = rnd.Next(1,  11);
+    }
+}
+
+for (int i =  0; i < originalMatrix.GetLength(0); i++)
+{
+    for (int j =  0; j < originalMatrix.GetLength(1); j++)
+    {
+        Console.Write($"{originalMatrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int[,] matrix = new int[rowCount, columnCount];
+
+    Random rnd = new Random();
+    for (int i =  0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =  0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1,  11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i =  0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =  0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Проверим работоспособность нашей функции.
+// Для этого создадим переменную matrix для хранения массива.
+// И вызовем функцию CreateMatrix.
+int[,] matrix = CreateMatrix(4,  5);
+ShowMatrix(matrix);
