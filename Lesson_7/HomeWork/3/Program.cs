b2cfc5c649c13d763b5e30422f8593b615c883c5
liza <li.belobor@yandex.ru>
2024-02-10 20:53:27 +0500

    add lecture_4 4.1

---
+++ Lesson_7/HomeWork/3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 8, 11, 4, 2 };
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index < 0) // base case: stop recursion when index is less than 0
+        {
+            return;
+        }
+        else
+        {
+            Console.WriteLine(arr[index]); // print the current element
+            PrintArrayReversed(arr, index - 1); // recursively call the function with the next index
+        }
+    }
+}
---
+++ lecture_4/4.1/Program.cs
@@ -0,0 +1,56 @@
+﻿int[,] originalMatrix = new int[4,  5];
+
+// Теперь сформируем 2 цикла, чтобы проходить по строкам массива (1й цикл)
+// И проходить по столбцам (2й цикл)
+// Чтобы в итоге заполнить каждый элемент значением.
+
+Random rnd = new Random();
+for (int i =  0; i < originalMatrix.GetLength(0); i++)
+{
+    for (int j =  0; j < originalMatrix.GetLength(1); j++)
+    {
+        originalMatrix[i, j] = rnd.Next(1,  11);
+    }
+}
+
+for (int i =  0; i < originalMatrix.GetLength(0); i++)
+{
+    for (int j =  0; j < originalMatrix.GetLength(1); j++)
+    {
+        Console.Write($"{originalMatrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int[,] matrix = new int[rowCount, columnCount];
+
+    Random rnd = new Random();
+    for (int i =  0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =  0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1,  11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i =  0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =  0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Проверим работоспособность нашей функции.
+// Для этого создадим переменную matrix для хранения массива.
+// И вызовем функцию CreateMatrix.
+int[,] matrix = CreateMatrix(4,  5);
+ShowMatrix(matrix);
