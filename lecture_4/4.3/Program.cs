dc1f9a683823923cc63432f176bdb31a96fd21d6
liza <li.belobor@yandex.ru>
2024-02-10 23:58:20 +0500

    add lecture_4 4.3

---
+++ lecture_4/4.3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Считать с консоли строку, состоящую из цифр и латинских букв
+// Сформировать новую строку, состоящую из букв исходной строки.
+
+// Действия:
+// Ввод строки с консоли
+// Проход по элементам строки
+// Проверка элемента - является ли он буквой?
+// Дописать подходящий элемент к новой строке
+
+string GetLettersFromString (string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if (char.IsLetter(e))
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
+
