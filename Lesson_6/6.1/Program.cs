e6638865b25d98f6bc766617a211a2d4e13c7ea0
liza <li.belobor@yandex.ru>
2024-02-17 22:01:51 +0500

    add Lesson_6 6.1

---
+++ Lesson_6/6.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Задайте массив символов (тип char []).
+// Создайте строку из символов этого массива.
+// Конструктор строки вида string(char []) не использовать.
+
+char[] muss = {'a', 'b', 'c', 'd'};
+string result = "";
+
+foreach (char i in muss)
+{
+    result = result + i;
+}
+Console.WriteLine(result);
