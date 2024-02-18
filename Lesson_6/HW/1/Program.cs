d47eaeb0cddac60d933b805684a77ede1246c6f2
liza <li.belobor@yandex.ru>
2024-02-18 11:05:01 +0500

    add Lesson_6 HW 1

---
+++ Lesson_6/HW/1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+char[,] muss = new char[,]
+{
+    {'a', 'b', 'c', 'd'},
+    {'t', 'e', 'n', 'k'},
+    {'u', 'i', 'd', 'w'}
+};
+
+string result = "";
+
+foreach (char i in muss)
+{
+    result = result + i;
+}
+Console.WriteLine(result);
